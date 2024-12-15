5699a7af6d2615d6c5c0421528fdf1feffa3f601
mendzek <mendzek10@gmail.com>
2024-12-15 18:26:14 +0300

    0.1

--- MainWin.py
+++ MainWin.py
@@ -4,14 +4,14 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter.messagebox import showerror, showwarning, showinfo
 import os
-import json
 import sqlite3
+import shutil
 
 root = Tk()
 root.title("UsTan")
 root.geometry("1200x700")
 
-DATA_FILE = "tests.json"
+MainUser = "mendzek"
 mainDir = "mainDir/"
 dirName = ""
 LBBList = list()
@@ -23,6 +23,7 @@ dirIconCursor = PhotoImage(file="dirIMG(gray).png")
 dirIconChosen = PhotoImage(file="dirIMG(chosen).png")
 clickedOrNot = False
 FilesList = list()
+FilesDict = {}
 
 
 LB_startText = Label(text = "Выберите папку", font = 20)
@@ -31,6 +32,8 @@ BT_viewReceived = tk.Button(text = "посмотреть какие файлы �
 BT_viewReceived.pack(anchor = NE)
 BT_choose = tk.Button(text="Выбрать")
 BT_choose.pack(anchor=SE)
+
+
 
 class SendWin(Tk):
     def __init__(self):
@@ -42,6 +45,43 @@ class SendWin(Tk):
         sqlPath = "database.db"
         self.UsersList = list()
         self.FilesList = FilesList
+        self.CheckingsDict = {}
+        self.SelectedUsersList = list()
+        self.SelectedFilePathsList = list()
+        for x in self.FilesList:
+            self.SelectedFilePathsList.append(FilesDict[x])
+
+        def onClickCheck(event):
+            self.tempLabel = event.widget
+            if self.tempLabel["text"] == "check":
+                self.tempLabel["text"] = "checked"
+                self.SelectedUsersList.append(self.CheckingsDict[self.tempLabel])
+            else:
+                self.tempLabel["text"] = "check"
+
+        def onClickSend(event):
+            self.connect = sqlite3.connect(sqlPath, timeout=5.0, detect_types=0,
+                                           isolation_level='DEFERRED', check_same_thread=True,
+                                           factory=sqlite3.Connection,
+                                           cached_statements=128, uri=False)
+            self.cursor = self.connect.cursor()
+            for x in self.SelectedUsersList:
+                self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {x["text"]} (id INTEGER NOT NULL, fileName TEXT, fromWho TEXT, PRIMARY KEY (id AUTOINCREMENT))")
+                self.connect.commit()
+                for y in self.FilesList:
+                    self.cursor.execute(f"INSERT INTO {x["text"]} (fileName, fromWho) VALUES (\"{y["text"]}\", \"{MainUser}\")")
+                    self.connect.commit()
+                    for z in self.SelectedFilePathsList:
+                        if not os.path.exists(mainDir+f"{x["text"]}"):
+                            os.makedirs(mainDir+f"{x["text"]}")
+                            shutil.copy(z, mainDir+f"{x["text"]}")
+                        else:
+                            shutil.copy(z, mainDir + f"{x["text"]}")
+            self.SuccessWin = SuccessWin()
+            self.destroy()
+
+
+
 
         self.connect = sqlite3.connect(sqlPath, timeout=5.0, detect_types=0,
                                        isolation_level='DEFERRED', check_same_thread=True, factory=sqlite3.Connection,
@@ -71,12 +111,30 @@ class SendWin(Tk):
             self.LB_userName.grid(column=1, row=x)
             self.BT_check = tk.Button(self.FrameUsers, text="check")
             self.BT_check.grid(column=2, row=x)
+            self.BT_check.bind("<ButtonRelease>", onClickCheck)
+            self.CheckingsDict[self.BT_check] = self.LB_userName
         self.FrameUsers.place(anchor=NW, x=40, y=100, width= 200, height=400)
         self.FrameFiles = ttk.Frame(self, borderwidth=1, relief=SOLID)
         for x in range(len(self.FilesList)):
             self.LB_fileName = Label(self.FrameFiles, text=self.FilesList[x]["text"], font=20)
             self.LB_fileName.grid(column=0, row=x)
         self.FrameFiles.place(anchor=NW, x=800, y=100, width=200, height=400)
+
+class SuccessWin(Tk):
+    def __init__(self):
+        super().__init__()
+
+        def onClickClose(event):
+            self.destroy()
+
+        self.title("UsTan")
+        self.geometry("1000x500")
+        self.LB_MainText = Label(self,text="Успешно отправлено")
+        self.LB_MainText.pack(anchor=CENTER)
+        self.BT_Close = ttk.Button(self, text="Выбрать новые файлы для отправки(назад)")
+        self.BT_Close.pack(anchor=S)
+        self.BT_Close.bind("<ButtonRelease>", onClickClose)
+
 
 
 def CheckFileType(dirOrFile):
@@ -98,9 +156,6 @@ def onClickFiles(event):
 
 def onClickChoose(event):
     sendWin = SendWin()
-
-def onClickSend(event):
-    pass #создать окно результата отправки
 
 def onClickViewReceived(event):
     pass
@@ -135,18 +190,19 @@ if dirName != "":
         #temp = os.path.abspath(x)
         tempDir = os.getcwd()
         if CheckFileType(tempFile) == "file":
-            labelInList = tk.Label(image = fileIcon, text = f"{x} file", compound = "top", width = 100 ,height = 100) # сделать серый эффект при наведении
+            labelInList = tk.Label(image = fileIcon, text = f"{x}", compound = "top", width = 100 ,height = 100)
             labelInList.bind("<Enter>", enterLeaveFile)
             labelInList.bind("<Leave>", enterLeaveFile)
             labelInList.bind("<ButtonRelease>", onClickFiles)
             labelInList.pack(padx = 10,pady = 10, side = LEFT)
             LBBList.append(labelInList)
+            FilesDict[labelInList] = tempFile
             c+=1
             if c == 4:
                 c=0
                 r+=1
         elif CheckFileType(tempFile) == "dir":
-            labelInList = tk.Label(image = dirIcon, text = f"{x} directory",compound = "top", width = 100 ,height = 100) # сделать серый эффект при наведении
+            labelInList = tk.Label(image = dirIcon, text = f"{x} directory",compound = "top", width = 100 ,height = 100)
             labelInList.bind("<Enter>", enterLeaveDir)
             labelInList.bind("<Leave>", enterLeaveDir)
             labelInList.pack(padx = 10,pady = 10, side = LEFT)
